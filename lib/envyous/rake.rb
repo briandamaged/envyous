79cd63756f7088ca1ad0cfe22fa24be9b589154b
Brian Lauber <blauber@jibe.com>
2014-12-11 22:06:30 -0500

    Defined rake tasks for creating config folder

--- lib/envyous/rake.rb
+++ lib/envyous/rake.rb
@@ -1,1 +1,66 @@
+require 'rake'
 
+require 'fileutils'
+
+module Envyous
+  module Rake
+    extend ::Rake::DSL
+
+    class AlreadyInitialized < Exception
+    end
+
+    class ConfigInitializer
+      attr_reader :src, :dest
+
+      def initialize(options = {})
+        @dest  = options.fetch(:dest)
+        @src   = options[:src]
+
+        unless @src
+          @src = "#{dest}.template"
+        end
+      end
+
+      def init(force = false)
+        if File.exists?(dest)
+          if force
+            FileUtils.rm_rf dest
+          else
+            raise AlreadyInitialized
+          end
+        end
+
+        FileUtils.cp_r src, dest
+      end
+
+      def soft_init
+        unless File.exists? dest
+          FileUtils.cp_r src, dest
+        end
+      end
+
+    end
+
+
+    def self.default!(options = {})
+      namespace :config do 
+        create_tasks!(options)
+      end
+    end
+
+    def self.create_tasks!(options = {})
+      cfg_init = ConfigInitializer.new(options)
+
+      desc "Initializes envyous config"
+      task :init do
+        cfg_init.init(ENV['force'] == "true")
+      end
+
+      desc "Soft-initializes envyous config"
+      task :soft_init do
+        cfg_init.soft_init
+      end
+    end
+
+  end
+end
